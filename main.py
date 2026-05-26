ad13bc04231fea1b6d0a1b84ba60382daeb505cc
phrosyne <mcoderej@gmail.com>
2026-05-26 16:54:59 -0700

    got an array to store the time but a bit unsure about how it deals with the hour holding two digits.

--- main.py
+++ main.py
@@ -4,30 +4,40 @@ from selenium.webdriver.common.by import By
 #KEEP BROWSER OPEN
 # options = webdriver.ChromeOptions()
 # options.add_experimental_option("detach", True)
-driver=webdriver.Chrome()
+CommunicationCenter = "Los Angeles"
+time = []
+driver = webdriver.Chrome()
+
 driver.get("https://cad.chp.ca.gov/")
 
 def reach_page():
     firstBox = driver.find_element(by=By.NAME, value="ddlComCenter")
     submit = driver.find_element(by=By.NAME, value="btnCCGo")
     firstBox.click()
-    firstBox.send_keys("Los Angeles")
+    firstBox.send_keys(CommunicationCenter)
     submit.click()
     
     
 def accident_type():
     return driver.find_elements(by=By.TAG_NAME, value="td")
     
-    
-events = []
-
 driver.implicitly_wait(0.5)
 
 reach_page()
 list = accident_type()
 
-allText = ""
-for row in list:
-    allText = allText + f"{row.text}"
+allFields = ""
+for cell in list:
+    allFields = allFields + f"{cell.text}"
     
-print(allText)
+row = allFields.split("Details")
+
+len = len(row)
+
+#Trimming the No. from each row
+for i in range(0, len):
+    row[i] = row[i][4:]
+    time.append(row[i][:7])
+print(row)
+time.pop(0)
+print(time)
